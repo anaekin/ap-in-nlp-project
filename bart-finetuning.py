7f0485a3e73180cd8c00c608a5c332596777bed8
Animesh Jain <animesh.jain1203@gmail.com>
2024-12-04 17:25:29 +0100

    Modified code to dynamically add few hyperparameters and refactored

--- bart-finetuning.py
+++ bart-finetuning.py
@@ -144,33 +144,33 @@ def prepare_training_args(params, generation_config=None):
     warmup_steps = params["warmup_steps"]
     eval_steps = params["eval_steps"]
     save_steps = params["save_steps"]
+    lr_scheduler_type = params["lr_scheduler_type"]
 
     training_args = Seq2SeqTrainingArguments(
         output_dir=output_dir,
-        report_to="none",  # No logging to external tools
         save_total_limit=3,
-        logging_strategy="steps",
-        logging_steps=100,
-        weight_decay=weight_decay,  # Helps prevent overfitting
-        eval_strategy="steps",
-        eval_steps=eval_steps,
-        warmup_steps=warmup_steps,  # Adjust based on dataset size
-        save_strategy="steps",
-        save_steps=save_steps,
-        dataloader_drop_last=False,
-        fp16=True,  # Enable mixed precision for memory efficiency
-        predict_with_generate=True,  # Required for summarization tasks
+        report_to="none",  # No logging to external tools
         dataloader_num_workers=2,  # Prevent memory bottlenecks
+        fp16=True,  # Enable mixed precision for memory efficiency
+        logging_dir=f"{output_dir}/logs",
+        logging_strategy="steps",
+        eval_strategy="steps",
+        save_strategy="steps",
+        logging_steps=100,
+        predict_with_generate=True,  # Required for summarization tasks
         metric_for_best_model="rougeL",  # Use ROUGE-L for best model selection
-        greater_is_better=True,  # Higher ROUGE-L is better
         load_best_model_at_end=True,  # Load the best model at the end of training
-        lr_scheduler_type="linear",
-        learning_rate=learning_rate,  # Standard for fine-tuning large models
-        per_device_train_batch_size=batch_size,  # Maximize within GPU capacity
-        per_device_eval_batch_size=batch_size,
+        weight_decay=weight_decay,  # Helps prevent overfitting
+        eval_steps=eval_steps,  # Adjust based on dataset size
+        warmup_steps=warmup_steps,  # Adjust based on dataset size
+        save_steps=save_steps,  # Adjust based on dataset size
+        lr_scheduler_type=lr_scheduler_type,  # linear or cosine, tre different lr schedulers
+        learning_rate=learning_rate,  # Standard for fine-tuning large models, 5e-05
+        per_device_train_batch_size=batch_size,  # Default 8
+        per_device_eval_batch_size=batch_size,  # Default 8
         gradient_accumulation_steps=grad_accum_steps,  # Simulates larger batch size
-        num_train_epochs=num_train_epochs,
-        generation_config=generation_config,
+        num_train_epochs=num_train_epochs,  # Smaller batch size requires more epochs
+        generation_config=generation_config,  # For generation during evaluation
     )
     return training_args
 
@@ -193,6 +193,11 @@ def compute_metrics_function(eval_preds, tokenizer, metrics=all_metrics):
     decoded_labels = [
         "\n".join(sent_tokenize(label.strip())) for label in decoded_labels
     ]
+
+    print("decoded_preds: ", decoded_preds[0])
+    print("\n")
+    print("decoded_labels: ", decoded_labels[0])
+    print("\n")
 
     results = {}
     for metric_name in metrics:
@@ -285,6 +290,7 @@ def save_model(model, tokenizer, params):
 
 
 def save_params(params):
+    os.makedirs(os.path.dirname(params["params_save_path"]), exist_ok=True)
     # Save the parameters
     with open(params["params_save_path"], "w") as f:
         json.dump(params, f, indent=4)
@@ -395,66 +401,78 @@ def fine_tune_model(params, metrics=None):
 
 ################################################################################
 if __name__ == "__main__":
-    output_folder = "output"
-    params = {
-        "model_checkpoint": "facebook/bart-large-xsum",
-        "dataset_name": "knkarthick/dialogsum",
-        # Save folder paths
-        "model_training_output_path": f"./{output_folder}/training-output",
-        "model_save_path": f"./{output_folder}/finetuned-output",
-        "dataset_save_path": f"./{output_folder}/dataset-preprocessing-output",
-        "params_save_path": f"./{output_folder}/training_params.json",
-    }
 
-    # For Jupyter Lab - Best (Less performant but more accurate)
-    # Example calculation
-    # steps_per_epoch = total_train_examples / (batch_size * grad_accum_steps) = 12600 / (16 * 4) = 196.875
-    # total_steps = steps_per_epoch * num_train_epochs = 196.875 * 10 = ~1968
-    # eval_steps = total_steps * eval_steps_factor = 1968 * 0.1 = ~196
-    # warmup_steps = total_steps * warmup_steps_factor = 1968 * 0.2 = ~393
-    # save_steps = eval_steps * save_steps_multiple = 196 * 3 = 588
-    params["prefix"] = (
-        "Generate a summary for the following dialogue: "  # In case of T5 model, use 'summarize: '
-    )
-    params["max_source_length"] = (
-        512  # Check dataset-analysis.ipynb for max_source_length
-    )
-    params["max_target_length"] = (
-        90  # Check dataset-analysis.ipynb for max_target_length
-    )
-    params["weight_decay"] = 0.01
-    params["learning_rate"] = 5e-5
-    params["batch_size"] = 30
-    params["grad_accum_steps"] = 4
-    params["num_train_epochs"] = 6
-    params["save_steps_multiple"] = (
-        5  # If 5, the model will save after every 5 * eval_steps
-    )
-    params["warmup_steps_factor"] = (
-        0.1  # If 0.1, the model will warmup for 0.1 * total_steps
-    )
-    params["eval_steps_factor"] = (
-        0.1  # If 0.2, the model will eval after every 0.2 * steps_per_epoch
-    )
+    def get_params(
+        model_checkpoint,
+        model_output_folder,
+        dataset_output_folder,
+        with_generation_config=False,
+    ):
 
-    # For mobile GPU (RTX 3070 Ti) - Balanced Performance and Accuracy - Takes ~6 hours
-    # params["learning_rate"] = 3e-5
-    # params["batch_size"] = 2
-    # params["grad_accum_steps"] = 2
-    # params["num_train_epochs"] = 3
-
-    # params["generation_config"] = None
-    params["generation_config"] = {
-        "max_length": params["max_target_length"],
-        "num_beams": 6,
-        # "top_k": 50,
-        # "top_p": 0.9,
-        # "temperature": 0.8,
-        "no_repeat_ngram_size": 3,
-        "length_penalty": 2,
-        "do_sample": True,
-        "_from_model_config": False,
-    }
+        # For Jupyter Lab
+        # Example calculation for steps
+        ###############################
+        # steps_per_epoch = total_train_examples / (batch_size * grad_accum_steps) = 12600 / (16 * 4) = 196.875
+        # total_steps = steps_per_epoch * num_train_epochs = 196.875 * 10 = ~1968
+        # eval_steps = total_steps * eval_steps_factor = 1968 * 0.1 = ~196
+        # warmup_steps = total_steps * warmup_steps_factor = 1968 * 0.2 = ~393
+        # save_steps = eval_steps * save_steps_multiple = 196 * 3 = 588
+        ##############################
+        params = {
+            # Save folder paths
+            "model_checkpoint": model_checkpoint,
+            "dataset_name": "knkarthick/dialogsum",
+            "model_training_output_path": f"./{model_output_folder}/training-output",
+            "model_save_path": f"./{model_output_folder}/finetuned-output",
+            "params_save_path": f"./{model_output_folder}/training_params.json",
+            "dataset_save_path": f"./{dataset_output_folder}/dataset-preprocessing-output",
+            # Hyperparameters
+            "prefix": "",  # In case of T5 model, use 'summarize: '
+            "max_source_length": 512,  # Check dataset-analysis.ipynb for max_source_length
+            "max_target_length": 90,  # Check dataset-analysis.ipynb for max_target_length
+            "weight_decay": 0.01,
+            "learning_rate": 5e-5,
+            "batch_size": 8,
+            "grad_accum_steps": 8,
+            "num_train_epochs": 15,
+            "lr_scheduler_type": "cosine",
+            "warmup_steps_factor": 0.2,  # If 0.1, the model will warmup for 0.1 * total_steps
+            "eval_steps_factor": 0.1,  # If 0.2, the model will eval after every 0.2 * total_steps
+            "save_steps_multiple": 3,  # If 5, the model will save after every 5 * eval_steps
+            "generation_config": (
+                {
+                    "_from_model_config": False,  # Needed to show that this is manual generation config
+                    "max_length": params["max_target_length"],
+                    "min_length": 1,
+                    "num_beams": 6,
+                    "no_repeat_ngram_size": 3,
+                    "length_penalty": 1.4,
+                    # "top_k": 50,
+                    # "top_p": 0.9,
+                    # "temperature": 0.8,
+                }
+                if with_generation_config
+                else None
+            ),
+        }
 
     # Fine-tuning the model ##############
+    # Change this only if you are changing tokenization process, max_source_length, or max_target_length
+    dataset_output_folder = "tokenized_dataset"
+
+    # Model to load and fine-tune
+    model_checkpoint = "facebook/bart-large-xsum"
+    # model_checkpoint = "facebook/bart-large-cnn"
+    # model_checkpoint = "facebook/bart-base"
+    model_output_folder = "model_output"
+
+    # To add generation config
+    with_generation_config = True
+
+    params = get_params(
+        model_checkpoint,
+        model_output_folder,
+        dataset_output_folder,
+        with_generation_config,
+    )
     fine_tune_model(params, metrics=["rouge", "bleu"])
